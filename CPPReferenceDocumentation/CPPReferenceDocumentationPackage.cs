0afaf2c4ee0e786c139154dd0ee8bc6bd712e3f7
LazyPanda07 <semengricenko@gmail.com>
2021-03-26 22:00:50 +0500

    Added shortcut for GetDescription command, getting current selected text

--- CPPReferenceDocumentation/CPPReferenceDocumentationPackage.cs
+++ CPPReferenceDocumentation/CPPReferenceDocumentationPackage.cs
@@ -25,6 +25,7 @@ namespace CPPReferenceDocumentation
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(CPPReferenceDocumentationPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class CPPReferenceDocumentationPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace CPPReferenceDocumentation
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await CPPReferenceDocumentation.Commands.GetDescription.InitializeAsync(this);
         }
 
         #endregion
